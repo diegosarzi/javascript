f2c19f085ad6c8a87bad25bde04d841daaf24dcc
Diego Sarzi <diego.sarzi@pixter.com.br>
2020-01-26 17:45:04 -0300

    objeto2

---
+++ udemy/objeto2.js
@@ -0,0 +1,10 @@
+console.log(typeof Object)
+console.log(typeof new Object)
+
+const Client = function () {}
+console.log(typeof Client)
+console.log(typeof new Client)
+
+class Produto {}
+console.log(typeof Produto)
+console.log(typeof new Produto)
