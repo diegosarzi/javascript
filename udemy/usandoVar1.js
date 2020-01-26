5d937ab9d9138c5b423d78c5baf5194c7d8cb301
Diego Sarzi <diego.sarzi@pixter.com.br>
2020-01-26 15:58:51 -0300

    usando var

---
+++ udemy/usandoVar1.js
@@ -0,0 +1,22 @@
+{
+    {
+        {
+            {
+                var sera = "Sera???"
+                console.log(sera)
+            }
+        }
+    }
+}
+
+console.log(sera)
+
+function teste(){
+    var local = 123
+    console.log(local)
+}
+
+teste()
+
+// não vai imprimrir pois está dentro de uma funcao
+console.log(local)
